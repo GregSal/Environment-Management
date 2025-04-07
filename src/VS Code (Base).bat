243cb9222245455c321f7d3ae469813958a08908
Greg Salomons <smokey.salomons@gmail.com>
2025-04-07 15:21:18 -0400

    Created module_dependency_analysis

--- src/VS Code (Base).bat
+++ src/VS Code (Base).bat
@@ -1,4 +1,5 @@
 CALL C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
-CD "D:\OneDrive - Queen's University\Python\Python Learning\Environment Management"
-D:
+REM CD "D:\OneDrive - Queen's University\Python\Python Learning\Environment Management"
+CD "C:\Users\gsalomon\OneDrive - Kingston Health Sciences Centre\Greg Salomons Workspace\Python Scripts\Environment Management"
+REM D:
 code .
